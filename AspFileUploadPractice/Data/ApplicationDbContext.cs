b69ec694f08e3f349d138fd4692724a5922ac559
Bryan Siegel <bryansiegel@gmail.com>
2024-05-16 11:42:33 -0700

    Add project files.

---
+++ AspFileUploadPractice/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AspFileUploadPractice.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
